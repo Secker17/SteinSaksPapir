e7d522b3d05c7279f0fff50f46037ca96354a636
Secker17 <96775895+Secker17@users.noreply.github.com>
2023-09-04 14:55:49 +0200

    Update spill.js

--- Secker consulting/Spill/spill.js
+++ Secker consulting/Spill/spill.js
@@ -1,8 +1,30 @@
 let countdown;
 let playerScore = 0;
 let botScore = 0;
+let allowChoice = false;
+
+const winSound = new Audio('sounds/win.mp3');
+const loseSound = new Audio('sounds/lose.mp3');
+const tieSound = new Audio('sounds/tie.mp3');
 
 function makeChoice(playerChoice) {
+    if (!allowChoice) {
+        document.getElementById("result").textContent = "For tidlig! Bot vinner denne runden.";
+        botScore++;
+        updateScore();
+        loseSound.play();
+        if (botScore === 2) {
+            setTimeout(() => {
+                alert("Beklager, bot vant.");
+                location.reload();
+            }, 500);
+        } else {
+            clearTimeout(countdown);
+            startCountdown();
+        }
+        return;
+    }
+
     let choices = ['stein', 'saks', 'papir'];
     let botChoice = choices[Math.floor(Math.random() * 3)];
     clearTimeout(countdown);
@@ -10,10 +32,14 @@ function makeChoice(playerChoice) {
         let winner = getWinner(playerChoice, botChoice);
         if (winner === 'player') {
             playerScore++;
+            winSound.play();
         } else if (winner === 'bot') {
             botScore++;
+            loseSound.play();
+        } else {
+            tieSound.play();
         }
-
+        updateScore();
         document.getElementById("result").textContent = `Du valgte: ${playerChoice}. Bot valgte: ${botChoice}. ${winner === 'tie' ? 'Uavgjort!' : winner === 'player' ? 'Du vant!' : 'Bot vant!'}`;
 
         if (playerScore === 2 || botScore === 2) {
@@ -45,14 +71,20 @@ function getWinner(playerChoice, botChoice) {
 
 function startCountdown() {
     let timer = 3;
+    allowChoice = false;  
     document.getElementById("timer").textContent = timer;
     countdown = setInterval(() => {
         timer--;
+        if (timer === 1) {
+            allowChoice = true;  
+        }
         document.getElementById("timer").textContent = timer;
         if (timer === 0) {
             clearTimeout(countdown);
             document.getElementById("result").textContent = "For sent! Bot vinner denne runden.";
             botScore++;
+            updateScore();
+            loseSound.play();
             if (botScore === 2) {
                 setTimeout(() => {
                     alert("Beklager, bot vant.");
@@ -65,10 +97,12 @@ function startCountdown() {
     }, 1000);
 }
 
-document.getElementById("startGame").addEventListener("click", function() {
-    // Starter nedtellingen når knappen trykkes
-    startCountdown();
+function updateScore() {
+    document.getElementById("score").textContent = `${playerScore} - ${botScore}`;
+}
 
-    // Skjuler knappen etter at den er trykket
+document.getElementById("startGame").addEventListener("click", function() {
+    updateScore();
+    startCountdown();
     this.style.display = 'none';
 });
