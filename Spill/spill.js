3ba7428f6e7db80ee27119814eb6f00f14521e94
Secker17 <96775895+Secker17@users.noreply.github.com>
2023-10-09 10:22:00 +0200

    Update spill.js

--- Spill/spill.js
+++ Spill/spill.js
@@ -19,55 +19,16 @@ function makeChoice(playerChoice) {
                 location.reload();
             }, 500);
         } else {
-            clearTimeout(countdown);
+            clearInterval(countdown); 
             startCountdown();
         }
         return;
     }
 
-    let choices = ['stein', 'saks', 'papir'];
-    let botChoice = choices[Math.floor(Math.random() * 3)];
-    clearTimeout(countdown);
-    countdown = setTimeout(() => {
-        let winner = getWinner(playerChoice, botChoice);
-        if (winner === 'player') {
-            playerScore++;
-            winSound.play();
-        } else if (winner === 'bot') {
-            botScore++;
-            loseSound.play();
-        } else {
-            tieSound.play();
-        }
-        updateScore();
-        document.getElementById("result").textContent = `Du valgte: ${playerChoice}. Bot valgte: ${botChoice}. ${winner === 'tie' ? 'Uavgjort!' : winner === 'player' ? 'Du vant!' : 'Bot vant!'}`;
 
-        if (playerScore === 2 || botScore === 2) {
-            setTimeout(() => {
-                alert(playerScore === 2 ? "Gratulerer, du vant!" : "Beklager, bot vant.");
-                location.reload();
-            }, 500);
-        } else {
-            startCountdown();
-        }
-    }, 3000);
+    clearTimeout(countdown);  
 }
 
-function getWinner(playerChoice, botChoice) {
-    if (playerChoice === botChoice) {
-        return 'tie';
-    }
-
-    if (
-        (playerChoice === 'stein' && botChoice === 'saks') ||
-        (playerChoice === 'saks' && botChoice === 'papir') ||
-        (playerChoice === 'papir' && botChoice === 'stein')
-    ) {
-        return 'player';
-    }
-
-    return 'bot';
-}
 
 function startCountdown() {
     let timer = 3;
@@ -80,22 +41,27 @@ function startCountdown() {
         }
         document.getElementById("timer").textContent = timer;
         if (timer === 0) {
-            clearTimeout(countdown);
-            document.getElementById("result").textContent = "For sent! Bot vinner denne runden.";
-            botScore++;
-            updateScore();
-            loseSound.play();
-            if (botScore === 2) {
-                setTimeout(() => {
-                    alert("Beklager, bot vant.");
-                    location.reload();
-                }, 500);
-            } else {
-                startCountdown();
+            clearInterval(countdown);  // Using clearInterval
+            if (allowChoice) {  // Check if player hasn't chosen
+                document.getElementById("result").textContent = "For sent! Bot vinner denne runden.";
+                botScore++;
+                updateScore();
+                loseSound.play();
+                if (botScore === 2) {
+                    setTimeout(() => {
+                        alert("Beklager, bot vant.");
+                        location.reload();
+                    }, 500);
+                } else {
+                    startCountdown();
+                }
             }
         }
     }, 1000);
 }
+
+// ... rest of the code remains the same.
+
 
 function updateScore() {
     document.getElementById("score").textContent = `${playerScore} - ${botScore}`;
