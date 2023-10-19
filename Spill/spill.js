71498ec06ec30cd68d7582324879fed159df6f8d
Secker17 <96775895+Secker17@users.noreply.github.com>
2023-10-19 08:51:18 +0200

    Update spill.js

--- Spill/spill.js
+++ Spill/spill.js
@@ -24,25 +24,60 @@ function makeChoice(playerChoice) {
         }
         return;
     }
+    clearTimeout(countdown);
 
-
-    clearTimeout(countdown);  
+    const botChoice = getBotChoice();
+    if (playerChoice === botChoice) {
+        tieSound.play();
+        document.getElementById("result").textContent = "Uavgjort!";
+        setTimeout(() => {
+            startCountdown();
+        }, 500);
+    } else if (isPlayerWinner(playerChoice, botChoice)) {
+        playerScore++;
+        winSound.play();
+        updateScore();
+        document.getElementById("result").textContent = "Du vant denne runden!";
+        if (playerScore === 2) {
+            setTimeout(() => {
+                alert("Gratulerer! Du vant.");
+                location.reload();
+            }, 500);
+        } else {
+            setTimeout(() => {
+                startCountdown();
+            }, 500);
+        }
+    } else {
+        botScore++;
+        loseSound.play();
+        document.getElementById("result").textContent = "Bot vant denne runden.";
+        if (botScore === 2) {
+            setTimeout(() => {
+                alert("Beklager, bot vant.");
+                location.reload();
+            }, 500);
+        } else {
+            setTimeout(() => {
+                startCountdown();
+            }, 500);
+        }
+    }
 }
-
 
 function startCountdown() {
     let timer = 3;
-    allowChoice = false;  
+    allowChoice = false;
     document.getElementById("timer").textContent = timer;
     countdown = setInterval(() => {
         timer--;
         if (timer === 1) {
-            allowChoice = true;  
+            allowChoice = true;
         }
         document.getElementById("timer").textContent = timer;
         if (timer === 0) {
-            clearInterval(countdown);  // Using clearInterval
-            if (allowChoice) {  // Check if player hasn't chosen
+            clearInterval(countdown);
+            if (allowChoice) {
                 document.getElementById("result").textContent = "For sent! Bot vinner denne runden.";
                 botScore++;
                 updateScore();
@@ -60,8 +95,6 @@ function startCountdown() {
     }, 1000);
 }
 
-
-
 function updateScore() {
     document.getElementById("score").textContent = `${playerScore} - ${botScore}`;
 }
@@ -71,3 +104,15 @@ document.getElementById("startGame").addEventListener("click", function() {
     startCountdown();
     this.style.display = 'none';
 });
+
+function getBotChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function isPlayerWinner(playerChoice, botChoice) {
+    if (playerChoice === "rock" && botChoice === "scissors") return true;
+    if (playerChoice === "paper" && botChoice === "rock") return true;
+    if (playerChoice === "scissors" && botChoice === "paper") return true;
+    return false;
+}
